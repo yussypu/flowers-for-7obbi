d67c75e334dcf3947becb5df66d1e5f4ed5355eb
4d616c616b <124023463+yussypu@users.noreply.github.com>
2024-03-08 19:23:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add a sweet message
+const message = "Sorry I can't give you actual flowers 8868 km away, but I hope these will do.";
+
+// Display the message in the console (optional)
+console.log(message);
+
+// Display an alert with the message
+alert(message);
